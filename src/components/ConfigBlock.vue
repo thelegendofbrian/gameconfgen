1b3ccd7b2156253f14141cf48141ffc5d5941097
Brian <thelegendofbrian@gmail.com>
2020-06-21 00:02:05 -0700

    Inputs read JSON properly

--- src/components/ConfigBlock.vue
+++ src/components/ConfigBlock.vue
@@ -4,28 +4,28 @@
     <p>{{ config.configDesc }}</p>
 
     <template v-if="config.displayType === 0">
-      <label>{{ config.options[sliderValue].name }}</label>
+      <label>{{ config.options[sliderValue].name }}</label><br>
       <input type="range" min=0 :max="config.options.length-1" v-model="sliderValue" >
     </template>
 
     <template v-if="config.displayType === 1">
       <span v-for="item in config.options" :key="item.name">
-        <input type="radio" name="radio" value="item.name"/>
+        <input type="radio" :name="config.configName" :value="item.name" v-model="radioValue"/>
         <label>{{ item.name }}</label><br>
       </span>
     </template>
 
     <template v-if="config.displayType === 2">
-      <input type="text" />
+      <input type="text" v-model="textValue" />
     </template>
 
     <template v-if="config.displayType === 3">
-      <input type="checkbox" />
+      <input type="checkbox" :id="config.configName" v-model="checkboxValue" />
     </template>
 
     <template v-if="config.displayType === 4">
-      <select>
-        <option v-for="item in config.options" :key="item.name" value="">
+      <select v-model="selectValue">
+        <option v-for="item in config.options" :key="item.name" :value="item.name">
           {{ item.name }}
         </option>
       </select>
@@ -41,8 +41,16 @@ export default {
     config: Object
   },
   data: function() {
-    return {
-      sliderValue:2
+    if (this.config.displayType === 0) {
+      return {sliderValue:this.config.defaultOption}
+    } else if (this.config.displayType === 1) {
+      return {radioValue:this.config.options[this.config.defaultOption].name}
+    } else if (this.config.displayType === 2) {
+      return {textValue:this.config.defaultOption}
+    } else if (this.config.displayType === 3) {
+      return {checkboxValue:this.config.options[this.config.defaultOption].name}
+    } else if (this.config.displayType === 4) {
+      return {selectValue:this.config.options[this.config.defaultOption].name}
     }
   }
 }
